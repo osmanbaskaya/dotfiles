90f746352858af4780d548e3434d361a2762617c
Osman Baskaya <thyme.exe@gmail.com>
2019-09-22 11:06:53 -0700

    diff-so-fancy

--- installation.sh
+++ installation.sh
@@ -30,3 +30,20 @@ cp byobu.tmux.conf ~/.byobu/tmux.conf
 mkdir -p $HOME/.config/byobu/
 cp byobu.tmux.conf $HOME/.config/byobu/.tmux.conf
 set -e
+
+# Git diff setup
+brew install diff-so-fancy
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
